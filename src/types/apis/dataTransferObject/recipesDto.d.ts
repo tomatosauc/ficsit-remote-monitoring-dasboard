2d06527834de050559a284461a72f776b6cbd968
tomatosauc <tomino.mesaros@gmail.com>
2026-07-17 16:07:11 +0200

    update recipe Dto

--- src/types/apis/dataTransferObject/recipesDto.d.ts
+++ src/types/apis/dataTransferObject/recipesDto.d.ts
@@ -15,7 +15,7 @@ export type RecipesDto = {
   Events?: (string | null)[] | null;
   Ingredients?: (ItemsEntity | null)[] | null;
   Products?: (ItemsEntity | null)[] | null;
-  ProducedIn?: string;
+  ProducedIn?: (string | null) | null;
   ManualDuration: number;
   FactoryDuration: number; 
 };
