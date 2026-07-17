bf4de66ea16818784a8ed8f5cac7bd7a021a4f78
tomatosauc <tomino.mesaros@gmail.com>
2026-07-17 15:56:08 +0200

    Create recipe Dto to Fm mapper

---
+++ src/mappers/recipeDtoToFmMapper.mapper.ts
@@ -0,0 +1,22 @@
+import type { RecipesDto } from "../types/apis/dataTransferObject/recipesDto";
+import type { RecipesFm } from "../types/apis/frontModel/recipesFm"; 
+import { enumDtoToFmMapper } from "./enumDtoToFm.mapper";
+import { GameClassNamesEnum } from "../enums/gameClassNames.enum";
+
+export const recipesDtoToFmMapper = (dto: RecipesDto[],): RecipesFm[] => {
+  return dto.map((recipesDto) => {
+    const ClassName = enumDtoToFmMapper(
+			recipesDto.ClassName,
+			GameClassNamesEnum,
+			"GameClassNamesEnum",
+    );
+    
+    return {
+			Name: recipesDto.Name,
+			ClassName,
+			Category: recipesDto.Category,
+			ManualDuration: recipesDto.ManualDuration,
+			FactoryDuration: recipesDto.FactoryDuration,
+    }
+  });
+};
