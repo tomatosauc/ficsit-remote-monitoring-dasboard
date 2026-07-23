25ced25ac770fdc476b4f89a685e5b02c3761ada
tomatosauc <tomino.mesaros@gmail.com>
2026-07-23 19:00:22 +0200

    Add pages to factory view to reduce browser lag with too many objects on screen

--- src/react/views/factoryView.tsx
+++ src/react/views/factoryView.tsx
@@ -7,7 +7,7 @@ import {
   Grid,
   Skeleton,
   Stack,
-  Typography,
+  Typography
 } from "@mui/joy";
 import React, { useState } from "react";
 import { BsArrowRightShort, BsExclamationTriangleFill } from "react-icons/bs";
@@ -30,6 +30,8 @@ import { EndpointEnum } from "../../enums/endpoint.enum";
 import { RecipesDto } from "../../types/apis/dataTransferObject/recipesDto";
 import { RecipesFm } from "../../types/apis/frontModel/recipesFm";
 import { GameClassNamesEnum } from "../../enums/gameClassNames.enum";
+
+import { JoyPagination } from "../components/pagination";
 
 export const DetailedFactoryView: React.FC = () => {
   const [params] = useSearchParams();
@@ -72,7 +74,31 @@ export const DetailedFactoryView: React.FC = () => {
   const [productFilter, writeProductFilter] = useState<string[]>([]);
   const [statusFilter, writeStatusFilter] = useState<boolean[]>([]);
 
-  const [pageNumber, writePageNumber] = useState<number>(0);
+  const [pageNumber, writePageNumber] = useState<number>(1);
+
+  function pageChange(_: any, page: number) {
+    writePageNumber(page);
+  }
+
+  function recipeFilterChange(newState: string[]) {
+    writeRecipeFilter(newState);
+    writePageNumber(1);
+  }
+
+  function ingredientFilterChange(newState: string[]) {
+    writeIngredientFilter(newState);
+    writePageNumber(1);
+  }
+
+  function productFilterChange(newState: string[]) {
+    writeProductFilter(newState);
+    writePageNumber(1);
+  }
+
+  function statusFilterChange(newState: boolean[]) {
+    writeStatusFilter(newState);
+    writePageNumber(1);
+  }
 
   const filteredFactories = factories?.filter((factory) => {
     return (
@@ -84,7 +110,9 @@ export const DetailedFactoryView: React.FC = () => {
       statusFilter.length>0
       ? statusFilter.includes(factory.isProducing)
       : true)
-  }).slice(pageNumber*30, (pageNumber+1)*30);
+  });
+  
+  const slicedFactories = filteredFactories?.slice((pageNumber-1)*30, (pageNumber)*30);
 
   return (
     <Container sx={{ paddingTop: "50px" }}>
@@ -136,33 +164,36 @@ export const DetailedFactoryView: React.FC = () => {
             <FilterButton
               label="Ingredients"
               options={[{label:"option1", value:"yay"},{label:"option2", value:"yoy"}]}
-              onChange={writeIngredientFilter}
+              onChange={ingredientFilterChange}
             />
             <FilterButton
               label="Products"
               options={[{label:'test', value:'what'}]}
-              onChange={writeProductFilter}
+              onChange={productFilterChange}
             />
             <FilterButton
               label="Recipes"
               options={recipe_list}
-              onChange={writeRecipeFilter}
+              onChange={recipeFilterChange}
             />
             <FilterButton
               label="Status"
               options={[{label:'Producing...', value:true}, {label:'Not producing', value:false}]}
-              onChange={writeStatusFilter}
+              onChange={statusFilterChange}
             />
           </Grid>
         </CardContent>
       </Card>
+      <Grid container justifyContent={'center'} marginBottom={3}>
+        <JoyPagination count={filteredFactories ? Math.ceil(filteredFactories.length/30) : 1} siblingCount={3} boundaryCount={2} onChange={pageChange}/>
+      </Grid>
 
-      {filteredFactories ? (
+      {slicedFactories ? (
         <Grid
           container
           spacing={3}
         >
-          {filteredFactories.map((factory) => {
+          {slicedFactories.map((factory) => {
             return (
               <Grid
                 xs={4}
